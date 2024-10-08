a634813145619acc308ced17e46e7c9320ba23ee
namrqthakaipa <53577056+namrqthakaipa@users.noreply.github.com>
2024-10-08 14:59:51 +0530

    Adding Day3 class

--- file1.sql
+++ file1.sql
@@ -25,5 +25,22 @@ insert into student
 values
 ("SS308","Shruthi","7899374379","Devloper");
 
+create table diabetes
+	(Pregnancies
+ varchar(7) primary key, Glucose varchar(25),BloodPressure varchar(10), 
+ SkinThickness varchar(15), Insulin varchar(10),BMI varchar(10), DiabetesPedigreeFunction varchar(10),
+ Age varchar(10), Outcome varchar(10)
+   );
+
+select * from diabetes ;
+select Glucose from diabetes ;
+
+select * from diabetes where BloodPressure = "70";
+select * from diabetes where Bloodpressure BETWEEN 50 AND 100 ; 
+select * from diabetes where Insulin like "1%" ;     -- wildcard 
+
+# To modify an existing table
+alter table diabetes add column (occupation varchar(10));
+
       
       
